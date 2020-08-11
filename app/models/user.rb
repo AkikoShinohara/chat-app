e1b053e40c4e0dfc8ad3c6a6ee55b0c4190ebe32
AkikoShinohara <69193404+AkikoShinohara@users.noreply.github.com>
2020-08-11 14:36:22 +0900

    プルダウンの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true
 
-  has_many :room_users
-  has_many :rooms, through: :room_users
+  has_many :user_rooms
+  has_many :rooms, through: :user_rooms
 end
